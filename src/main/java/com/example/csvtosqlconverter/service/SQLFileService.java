15fe3ad220a30acab7743ba04ac7297c39e2c747
lemersom <lemerfilho@gmail.com>
2024-12-19 14:47:02 -0300

    feat: sql writer for create files

---
+++ src/main/java/com/example/csvtosqlconverter/service/SQLFileService.java
@@ -0,0 +1,64 @@
+package com.example.csvtosqlconverter.service;
+
+import com.example.csvtosqlconverter.util.CsvUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@Service
+public class SQLFileService {
+
+    @Value("${sql.directory}")
+    private String sqlDirectory;
+
+    public String convertCsvToSQL(String csvFilePath) {
+        int lastSlashIndex = csvFilePath.lastIndexOf('/');
+        String csvFileName = csvFilePath.substring(lastSlashIndex + 1);
+        String pathWithoutCsvFile = csvFilePath.substring(0, lastSlashIndex + 1);
+        String csvFileWithoutExtension = csvFileName.split("\\.")[0];
+
+        String query = "";
+
+        if(csvFileName.contains("_") && csvFileName.contains("schema")) {
+            // SQL CREATE file
+            String tableName = csvFileName.split("_")[0];
+            query = handleCreateGenerator(csvFilePath, tableName);
+        } else {
+            // SQL INSERT file
+        }
+
+        String date = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new java.util.Date());
+        String sqlConvertedDirectory = sqlDirectory + File.separator + pathWithoutCsvFile;
+        String sqlFilePath = sqlConvertedDirectory + csvFileWithoutExtension + "_" + date + ".sql";
+
+        try {
+            Files.createDirectories(Paths.get(sqlConvertedDirectory));
+
+            FileWriter fw = new FileWriter(sqlFilePath);
+            fw.write(query);
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return sqlFilePath;
+    }
+
+    private String handleCreateGenerator(String csvFilePath, String tableName) {
+        try {
+            List<String[]> csvData = CsvUtil.readFromCsv(csvFilePath);
+
+            return SQLGenerator.generateCreate(tableName, csvData);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/test/java/com/example/csvtosqlconverter/SQLGeneratorTest.java
+++ src/test/java/com/example/csvtosqlconverter/SQLGeneratorTest.java
@@ -1,41 +1,31 @@
 package com.example.csvtosqlconverter;
 
-import com.example.csvtosqlconverter.service.SQLGenerator;
-import com.example.csvtosqlconverter.util.CsvUtil;
+import com.example.csvtosqlconverter.service.SQLFileService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 public class SQLGeneratorTest {
+
+    @Autowired
+    SQLFileService sqlFileService;
 
     @Test
     public void shouldGenerateCreate() {
         String csvFilePath = "./backup-test/testdb/2024-12-17_16-13-26/customers_schema.csv";
 
-        String date = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new java.util.Date());
-        String sqlFileName = "customers-" + date + ".sql";
-        File sqlFile = new File("./sql-test/" + sqlFileName);
-
         try {
-            List<String[]> csvData = CsvUtil.readFromCsv(csvFilePath);
+            String sqlFilePath = sqlFileService.convertCsvToSQL(csvFilePath);
 
-            String query = SQLGenerator.generateCreate("customers", csvData);
-
-            FileWriter fw = new FileWriter(sqlFile);
-            fw.write(query);
-            fw.close();
-
-            assertTrue(sqlFile.exists());
-
+            assertTrue(new File(sqlFilePath).exists());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
 
 }
